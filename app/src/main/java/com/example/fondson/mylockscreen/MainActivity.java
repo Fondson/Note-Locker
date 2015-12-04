c0a7aca2f823c96ca6e58400abe3a0e7fd9afe87
Fondson <fondsont@gmail.com>
2015-12-04 07:48:38 -0500

    init

---
+++ app/src/main/java/com/example/fondson/mylockscreen/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.fondson.mylockscreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON|
+                WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD|
+                WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED|
+                WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
