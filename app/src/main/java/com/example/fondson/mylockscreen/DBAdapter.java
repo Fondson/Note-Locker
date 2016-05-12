19dcc3d303fa4b27cbf773847cda6fae678b22cd
Fondson Tran <fondsont@gmail.com>
2016-05-11 20:17:59 -0400

    IMPORTANT now stores items in a database table instead of a text file

---
+++ app/src/main/java/com/example/fondson/mylockscreen/DBAdapter.java
@@ -0,0 +1,187 @@
+package com.example.fondson.mylockscreen;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+/**
+ * Created by Fondson on 2016-05-10.
+ */
+public class DBAdapter {
+    /////////////////////////////////////////////////////////////////////
+    //	Constants & Data
+    /////////////////////////////////////////////////////////////////////
+    // For logging:
+    private static final String TAG = "DBAdapter";
+
+    // DB Fields
+    public static final String KEY_ROWID = "_id";
+    public static final int COL_ROWID = 0;
+
+    public static final String KEY_ITEM = "item";
+
+    public static final int COL_ITEM = 1;
+
+
+    public static final String[] ALL_KEYS = new String[] {KEY_ROWID, KEY_ITEM};
+
+    // DB info: it's name, and the table we are using (just one).
+    public static final String DATABASE_NAME = "NoteLockerDB";
+    public static final String DATABASE_TABLE = "itemTable";
+
+    public static final int DATABASE_VERSION = 1;
+
+    private static final String DATABASE_CREATE_SQL =
+            "create table " + DATABASE_TABLE
+                    + " (" + KEY_ROWID + " integer primary key autoincrement, "
+                    + KEY_ITEM + " string not null"
+                    + ");";
+
+    // Context of application
+    private final Context context;
+
+    private DatabaseHelper myDBHelper;
+    private SQLiteDatabase db;
+
+    /////////////////////////////////////////////////////////////////////
+    //	Public methods:
+    /////////////////////////////////////////////////////////////////////
+
+    public DBAdapter(Context ctx) {
+        this.context = ctx;
+        myDBHelper = new DatabaseHelper(context);
+    }
+
+    // Open the database connection.
+    public DBAdapter open() {
+        db = myDBHelper.getWritableDatabase();
+        return this;
+    }
+
+    // Close the database connection.
+    public void close() {
+        myDBHelper.close();
+    }
+
+    // Add a new set of values to the database.
+    public long insertRow(String item) {
+		/*
+		 * CHANGE 3:
+		 */
+        // TODO: Update data in the row with new fields.
+        // TODO: Also change the function's arguments to be what you need!
+        // Create row's data:
+        ContentValues initialValues = new ContentValues();
+        initialValues.put(KEY_ITEM,item);
+
+        // Insert it into the database.
+        return db.insert(DATABASE_TABLE, null, initialValues);
+    }
+
+    // Delete a row from the database, by rowId (primary key)
+    public boolean deleteRow(long rowId) {
+        String where = KEY_ROWID + "=" + rowId;
+        return db.delete(DATABASE_TABLE, where, null) != 0;
+    }
+
+    public void deleteAll() {
+        Cursor c = getAllRows();
+        long rowId = c.getColumnIndexOrThrow(KEY_ROWID);
+        if (c.moveToFirst()) {
+            do {
+                deleteRow(c.getLong((int) rowId));
+            } while (c.moveToNext());
+        }
+        c.close();
+    }
+
+    // Return all data in the database.
+    public Cursor getAllRows() {
+        String where = null;
+        Cursor c = 	db.query(true, DATABASE_TABLE, ALL_KEYS,
+                where, null, null, null, null, null);
+        if (c != null) {
+            c.moveToFirst();
+        }
+        return c;
+    }
+
+    // Get a specific row (by rowId)
+    public Cursor getRow(long rowId) {
+        String where = KEY_ROWID + "=" + rowId;
+        Cursor c = 	db.query(true, DATABASE_TABLE, ALL_KEYS,
+                where, null, null, null, "by Id desc", null);
+        if (c != null) {
+            c.moveToFirst();
+        }
+        return c;
+    }
+
+    // Get a specific row (by item)
+    public Cursor getRow(String item){
+        String where=String.format(KEY_ITEM + "= %s",item);
+        Cursor c=db.query(DATABASE_TABLE,ALL_KEYS,where,null,null,null,null);
+        if (c != null) {
+            c.moveToFirst();
+        }
+        return c;
+    }
+
+    //checks if item exists in table already
+    public boolean exists(String item){
+        String where=String.format(KEY_ITEM + "= %s",item);
+        Cursor c=db.query(DATABASE_TABLE,ALL_KEYS,where,null,null,null,null);
+        if (c!=null){
+            return true;
+        }
+        return false;
+    }
+
+    // Change an existing row to be equal to new data.
+    public boolean updateRow(long rowId, String item) {
+        String where = KEY_ROWID + "=" + rowId;
+
+        // Create row's data:
+        ContentValues newValues = new ContentValues();
+        newValues.put(KEY_ITEM,item);
+
+        // Insert it into the database.
+        return db.update(DATABASE_TABLE, newValues, where, null) != 0;
+    }
+
+
+
+    /////////////////////////////////////////////////////////////////////
+    //	Private Helper Classes:
+    /////////////////////////////////////////////////////////////////////
+
+    /**
+     * Private class which handles database creation and upgrading.
+     * Used to handle low-level database access.
+     */
+    private static class DatabaseHelper extends SQLiteOpenHelper
+    {
+        DatabaseHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase _db) {
+            _db.execSQL(DATABASE_CREATE_SQL);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase _db, int oldVersion, int newVersion) {
+            Log.w(TAG, "Upgrading application's database from version " + oldVersion
+                    + " to " + newVersion + ", which will destroy all old data!");
+
+            // Destroy old database:
+            _db.execSQL("DROP TABLE IF EXISTS " + DATABASE_TABLE);
+
+            // Recreate new database:
+            onCreate(_db);
+        }
+    }
+}
--- app/src/main/java/com/example/fondson/mylockscreen/Item.java
+++ app/src/main/java/com/example/fondson/mylockscreen/Item.java
@@ -4,13 +4,17 @@ package com.example.fondson.mylockscreen;
  * Created by Fondson on 2015-12-21.
  */
 public class Item {
+    long id;
     String name = null;
     boolean selected = false;
 
-    public Item(String name, boolean selected) {
+    public Item(long id, String name, boolean selected) {
+        this.id=id;
         this.name = name;
         this.selected = selected;
     }
+    public long getId(){return this.id;}
+    public void setId(long id){this.id=id;}
     public String getName() {
         return this.name;
     }
