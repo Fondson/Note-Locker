13718bbe1711ec9d62de27b66e4d2baef4d99783
Fondson Tran <fondsont@gmail.com>
2016-07-01 14:18:04 -0400

    changed how to handle selecting wallpaper

--- app/src/main/java/com/dev/fondson/NoteLocker/SettingsActivity.java
+++ app/src/main/java/com/dev/fondson/NoteLocker/SettingsActivity.java
@@ -14,12 +14,20 @@ import android.os.Environment;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
+import com.theartofdev.edmodo.cropper.CropImage;
+
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class SettingsActivity extends AppCompatActivity {
     public static final String PREF_KEY_OFF="pref_key_off";
@@ -35,6 +43,45 @@ public class SettingsActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (data!=null) {
+            switch (requestCode) {
+                case CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE:
+
+                CropImage.ActivityResult result = CropImage.getActivityResult(data);
+                InputStream inputStream;
+                File wallpaperFile = new File(MainActivity.WALLPAPER_FULL_PATH);
+                try {
+                    inputStream = this.getContentResolver().openInputStream(result.getUri());
+                    BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+
+                    Bitmap bitmap = BitmapFactory.decodeStream(bufferedInputStream);
+                    //copyInputStreamToFile(inputStream,wallpaperFile);                        //Bitmap bitmap=data.getExtras().getParcelable("data");
+                    bitmap =Bitmap.createScaledBitmap(bitmap, 2048, 2048, true);
+                    Drawable wallpaper= new BitmapDrawable(getResources(), bitmap);
+                    MainActivity.getBackground().setBackground(wallpaper);
+                    //copyInputStreamToFile(inputStream,wallpaperFile);
+                    if (wallpaperFile.exists())
+                        wallpaperFile.delete();
+                    try {
+                        FileOutputStream out = new FileOutputStream(wallpaperFile);
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+                        out.flush();
+                        out.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                Intent intent = new Intent(this,MainActivity.class);
+                startActivity(intent);
+                break;
+            }
+        }
+    }
     public static class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
         private SeekBarPreference darkTintSeekBar;
         private SharedPreferences sharedPreferences;
@@ -60,12 +107,13 @@ public class SettingsActivity extends AppCompatActivity {
             wallpaperPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
                 public boolean onPreferenceClick(Preference preference) {
                     //requests permissions needed for users to select background image on M or above
-                    if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M
-                            && getContext().checkCallingOrSelfPermission("android.permission.READ_EXTERNAL_STORAGE")!= PackageManager.PERMISSION_GRANTED
-                            && getContext().checkCallingOrSelfPermission("android.permission.WRITE_EXTERNAL_STORAGE")!= PackageManager.PERMISSION_GRANTED){
-                        requestPermissions(MainActivity.perms, 200);
-                    }
-                    else{launchGalleryPicker();}
+//                    if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M
+//                            && getContext().checkCallingOrSelfPermission("android.permission.READ_EXTERNAL_STORAGE")!= PackageManager.PERMISSION_GRANTED
+//                            && getContext().checkCallingOrSelfPermission("android.permission.WRITE_EXTERNAL_STORAGE")!= PackageManager.PERMISSION_GRANTED){
+//                        requestPermissions(MainActivity.perms, 200);
+//                    }
+//                    else{launchGalleryPicker();}
+                    launchGalleryPicker();
                     return true;
                 }
 
@@ -98,33 +146,32 @@ public class SettingsActivity extends AppCompatActivity {
 
 
         //handles permission requests
-        @Override
-        public void onRequestPermissionsResult(int permsRequestCode, String[] permissions,int[] grantResults){
-            switch (permsRequestCode){
-                case 200:
-                    if (grantResults.length>0
-                            && grantResults[0] == PackageManager.PERMISSION_GRANTED
-                            && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
-                        launchGalleryPicker();
-                    }
-                    break;
-            }
-        }
+//        @Override
+//        public void onRequestPermissionsResult(int permsRequestCode, String[] permissions,int[] grantResults){
+//            switch (permsRequestCode){
+//                case 200:
+//                    if (grantResults.length>0
+//                            && grantResults[0] == PackageManager.PERMISSION_GRANTED
+//                            && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+//                        launchGalleryPicker();
+//                    }
+//                    break;
+//            }
+//        }
 
         private void launchGalleryPicker(){
-            File wallpaperPath = new File(MainActivity.WALLPAPER_PATH);
-            File wallpaper = new File(MainActivity.WALLPAPER_FULL_PATH);
-            try {
-                wallpaperPath.mkdirs();
-                wallpaper.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            Intent photoPickerIntent = new Intent(Intent.ACTION_PICK,
-                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+//            File wallpaperPath = new File(MainActivity.WALLPAPER_PATH);
+//            try {
+//                wallpaperPath.mkdirs();
+//                wallpaper.createNewFile();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+            Intent photoPickerIntent = new Intent(Intent.ACTION_GET_CONTENT);
             photoPickerIntent.setType("image/*");
-            photoPickerIntent.putExtra("crop", "true");
-            photoPickerIntent.putExtra("output", Uri.fromFile(wallpaper));
+            photoPickerIntent.putExtra("return-data", true);
+            //photoPickerIntent.putExtra("crop", "true");
+            //photoPickerIntent.putExtra("output", Uri.parse(MainActivity.WALLPAPER_FULL_PATH));
             startActivityForResult(photoPickerIntent,MainActivity.WALLPAPER_CODE);
         }
 
@@ -135,14 +182,34 @@ public class SettingsActivity extends AppCompatActivity {
                 switch (requestCode) {
                     //handles picture-cropping results
                     case MainActivity.WALLPAPER_CODE:
-                        Bitmap bitmap = BitmapFactory.decodeFile(MainActivity.WALLPAPER_FULL_PATH);
-                        bitmap =Bitmap.createScaledBitmap(bitmap, 2048, 2048, true);
-                        Drawable wallpaper=new BitmapDrawable(getResources(), bitmap);
-                        MainActivity.getBackground().setBackground(wallpaper);
-                        Intent intent = new Intent(getActivity(),MainActivity.class);
-                        startActivity(intent);
+//                        Bitmap bitmap = BitmapFactory.decodeFile(MainActivity.WALLPAPER_FULL_PATH);
+//                        Bitmap bitmap = null;
+//                        try {
+//                            bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), Uri.parse(MainActivity.WALLPAPER_FULL_PATH));
+//                        } catch (IOException e) {
+//                            e.printStackTrace();
+//                        }
+
+
+                        CropImage.activity(data.getData()).start(getActivity());
+                        //startActivity(intent);x
+
                         break;
                 }
+            }
+        }
+        private void copyInputStreamToFile( InputStream in, File file ) {
+            try {
+                OutputStream out = new FileOutputStream(file);
+                byte[] buf = new byte[1024];
+                int len;
+                while((len=in.read(buf))>0){
+                    out.write(buf,0,len);
+                }
+                out.close();
+                in.close();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
     }
